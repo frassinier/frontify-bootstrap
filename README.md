20170b7846de700f3cc574d58f2ab7a86f659e28
Fabien Rassinier <frassinier@talend.com>
2016-09-13 00:22:02 +0200

    typography

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # frontify-connector
+
 Sync patterns with Frontify
+
+## Configuration
+
+* Create a `.env` file at root
+* Add `FRONTIFY_API_KEY`
+* Add `FRONTIFY_PROJECT_ID`
+
+## Run
+
+* Run `node index.js`
--- index.js
+++ index.js
@@ -5,10 +5,11 @@ var frontifyApi = require('@frontify/frontify-api');
 var config = {
 	access_token: process.env.FRONTIFY_API_KEY,
   project: process.env.FRONTIFY_PROJECT_ID,
+	// dryRun: true,
 };
 
 var blob = [
-	'patterns/**/*.html'
+	'test/fixtures/patterns/**/*.json'
 ];
 
 var promise = frontifyApi.syncPatterns(config, blob)
